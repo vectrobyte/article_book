affacda147b2cc6ef108bbce353d5879ded9fda7
Dilip Bhattarai <dilip@coxel.online>
2019-04-29 15:21:43 +0545

    minor changes.

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-import NavigationStack from './NavigationStack';
+// import NavigationStack from './NavigationStack';
+//
+// const AppNavigator = NavigationStack;
+//
+// export default AppNavigator;
 
-class AppNavigator extends Component {
-  render() {
-    return (<NavigationStack />);
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Login from '../screens/Login';
+
+const RootStack = createStackNavigator(
+  {
+    Login: { screen: Login },
+  }, {
+    initialRouteName: 'Login'
   }
-}
+);
+
+const AppNavigator = createAppContainer(RootStack);
 
 export default AppNavigator;
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,12 +1,22 @@
 import {Text, View} from 'react-native';
-import React from 'react';
+import React, {Component} from 'react';
 
-const Login = () => {
-  return (
-    <View>
-      <Text>This is Login View.</Text>
-    </View>
-  );
-};
+// const Login = () => {
+//   return (
+//     <View>
+//       <Text>This is Login View.</Text>
+//     </View>
+//   );
+// };
+
+class Login extends Component{
+  render() {
+    return (
+      <View>
+        <Text>This is Login View.</Text>
+      </View>
+    );
+  }
+}
 
 export default Login;

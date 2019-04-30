85364e096b7d909abe4e755b663cfcebbfee6b7a
Dilip Bhattarai <dilip@coxel.online>
2019-04-30 12:51:03 +0545

    define styles

---
+++ app/styles/Custom.ts
@@ -0,0 +1,22 @@
+export default {
+  container: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    color: '#000000',
+  },
+  formControl: {
+    marginBottom: 10
+  },
+  label: {
+    marginBottom: 5
+  },
+  textBox: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 0,
+    paddingLeft: 10
+  },
+  title: {
+    fontSize: 30,
+  },
+};

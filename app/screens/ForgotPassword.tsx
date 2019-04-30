278c38dd0c228f3787565dd45195f517c215f626
Dilip Bhattarai <dilip@coxel.online>
2019-04-30 12:31:18 +0545

    add forgot password component.

---
+++ app/screens/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <View>
+      <Text>This is Forgot password View.</Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;

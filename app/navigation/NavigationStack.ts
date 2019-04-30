59c72f4d63268f3eb5b0d160bedf5b468b465874
Dilip Bhattarai <dilip@coxel.online>
2019-04-30 12:32:57 +0545

    add forgot password route.

--- app/navigation/NavigationStack.ts
+++ app/navigation/NavigationStack.ts
@@ -4,6 +4,7 @@ import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
 import Dashboard from '../screens/Dashboard';
 import Account from '../screens/Account';
+import ForgotPassword from '../screens/ForgotPassword';
 
 const NavigationStack = createStackNavigator(
   {
@@ -13,6 +14,10 @@ const NavigationStack = createStackNavigator(
     },
     SignUp: {
       screen: SignUp,
+      navigationOptions: { header: null, gesturesEnabled: false }
+    },
+    ForgotPassword: {
+      screen: ForgotPassword,
       navigationOptions: { header: null, gesturesEnabled: false }
     },
     Dashboard: {
